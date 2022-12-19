7a9a92e92008f174413cf798316548492f091eb7
Sergey <stu@dbbsoftware.com>
2022-12-19 15:49:13 +0200

    add admin medusa site to ADMIN_CORS

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,https://serene-hamster-71174b.netlify.app";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,http://localhost:4020,https://canary-medusa-mesh-integration.netlify.app,https://medusa-mesh-integration.netlify.app";
